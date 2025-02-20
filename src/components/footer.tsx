5aecb02e6887fa52b1d4572211791aa461dcd3ac
Rajiv Seelam <rajiv@betalectic.com>
2025-02-20 20:37:16 +0530

    Add LinkedIn link to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -41,6 +41,17 @@ export default function Footer() {
             <p className="ml-2 h-7">x</p>
           </a>
         </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/company/deep-domain-problems/"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">linkedin</p>
+          </a>
+        </li>
         {/* <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
